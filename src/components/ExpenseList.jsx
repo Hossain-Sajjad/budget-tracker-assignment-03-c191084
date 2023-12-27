2fca28ac848e2cc17e7e7b8d8f6c3d5673a787f3
Hossain-Sajjad <sunshinems58@gmail.com>
2023-12-28 04:20:34 +0600

    solved delete itam problem

---
+++ src/components/ExpenseList.jsx
@@ -0,0 +1,45 @@
+import { formatMoney } from "../utils/format-money";
+import { useEntries } from "../hooks/useEntries";
+
+export default function ExpenseList() {
+  const { entries ,setEntries } = useEntries();
+  const expenseEntries = entries.filter((entry) => entry.type === "expense");
+  return (
+    <div>
+      <h2 className="border-b pb-2 font-medium text-red-600">Expense</h2>
+
+      {expenseEntries.length === 0 && (
+        <p className="py-2.5 text-gray-600">There are no expenses.</p>
+      )}
+
+      <ul id="expense-list" className="divide-y">
+        {expenseEntries.map((item) => {
+          return (
+            <li key={item.id} className="py-2.5">
+              <div className="group flex justify-between gap-2 text-sm">
+                <span>{item.title}</span>
+                <div>
+                  <span className="text-red-600">
+                    -{formatMoney(item.value)}
+                  </span>
+                  <span className="ml-2 hidden cursor-pointer font-medium text-red-500 group-hover:inline-block"
+                            onClick={(e) => {
+                              e.preventDefault();
+                              console.log("submitted");
+                              console.log(entries);
+                              console.log(item.id);
+                              setEntries(entries.filter((entry) => entry.id !== item.id));
+                              console.log(entries);
+                            }}
+                  >
+                    Delete
+                  </span>
+                </div>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
